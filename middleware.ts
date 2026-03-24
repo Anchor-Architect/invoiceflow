cdc7f9a1156195893331ef566c9cf33419c573fc
Anchor_Architect <architect@anchors.work>
2026-03-24 12:56:32 +0700

    Deployment modified

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,13 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - /login
-     * - /api/auth (next-auth)
-     * - /_next (static)
-     * - /favicon.ico
-     */
     "/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)",
   ],
 };
